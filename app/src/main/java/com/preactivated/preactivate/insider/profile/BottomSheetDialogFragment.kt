b2f774bb24192c084e8977c4cb8a69e26b4efab0
DenoiserAi <devanshsaxena1019@gmail.com>
2024-03-16 03:56:22 +0530

    Init

--- app/src/main/java/com/preactivated/preactivate/insider/profile/BottomSheetDialogFragment.kt
+++ app/src/main/java/com/preactivated/preactivate/insider/profile/BottomSheetDialogFragment.kt
@@ -1,11 +1,16 @@
 package com.preactivated.preactivate.insider.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.auth.FirebaseAuth
 import com.preactivated.preactivate.R
+import com.preactivated.preactivate.starting.LoginActivity
 
 class BottomSheetDialogFragment : BottomSheetDialogFragment() {
 
@@ -14,6 +19,17 @@ class BottomSheetDialogFragment : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_bottom_sheet_dialog, container, false)
+        val view = inflater.inflate(R.layout.fragment_bottom_sheet_dialog, container, false)
+
+        val signOutButton = view.findViewById<LinearLayout>(R.id.signout)
+        signOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+            Toast.makeText(context, "Signed out successfully", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 }
