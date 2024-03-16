0e05d1b72f5f1a0039085eefb8c33f19a2233987
DenoiserAi <devanshsaxena1019@gmail.com>
2024-03-16 18:33:38 +0530

    Init

--- app/src/main/java/com/preactivated/preactivate/insider/profile/drawer/ReleaseDependencyActivity.kt
+++ app/src/main/java/com/preactivated/preactivate/insider/profile/drawer/ReleaseDependencyActivity.kt
@@ -12,10 +12,8 @@ class ReleaseDependencyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_release_dependency)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+
     }
 }
